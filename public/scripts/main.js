569ab9efc49a3c42a70eff90817745ba1716a773
jooskim <jsk9260@gmail.com>
2014-01-10 09:43:34 -0500

    layout change for bg values

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -74,6 +74,14 @@ define(['jquery','D3','queue','moment','slider'], function($, d3, queue, moment)
         }, 'bg')
         .await(run);
 
+    function unique(list) {
+        var result = [];
+        $.each(list, function(i, e) {
+            if ($.inArray(e, result) == -1) result.push(e);
+        });
+        return result;
+    }
+
     function structureData(){
         //console.log(raw);
         var datesAggregated = [];
@@ -85,9 +93,9 @@ define(['jquery','D3','queue','moment','slider'], function($, d3, queue, moment)
         // total number of weeks
         var totWeeks = moment(d3.max(datesAggregated)).week();
         for(var i=0; i<totWeeks - moment(d3.min(datesAggregated)).week() + 1; i++){
-            var tempLevels = [['sun',[]],['mon',[]],['tue',[]],['wed',[]],['thu',[]],['fri',[]],['sat',[]]];
-            var tempTimes = [['sun',[]],['mon',[]],['tue',[]],['wed',[]],['thu',[]],['fri',[]],['sat',[]]];
-            var tempDates = [['sun',[]],['mon',[]],['tue',[]],['wed',[]],['thu',[]],['fri',[]],['sat',[]]];
+            var tempLevels = [['sun',[]],['mon',[]],['tue',[]],['wed',[]],['thu',[]],['fri',[]],['sat',[]],['prevsat',[]]];
+            var tempTimes = [['sun',[]],['mon',[]],['tue',[]],['wed',[]],['thu',[]],['fri',[]],['sat',[]],['prevsat',[]]];
+            var tempDates = [['sun',[]],['mon',[]],['tue',[]],['wed',[]],['thu',[]],['fri',[]],['sat',[]],['prevsat',[]]];
             raw.forEach(function(data){
                 if(moment(data.createdDate).week()-(moment(d3.min(datesAggregated)).week()) == i){
                     if(moment(data.createdDate).day() == 0){
@@ -120,72 +128,31 @@ define(['jquery','D3','queue','moment','slider'], function($, d3, queue, moment)
                         tempDates[6][1].push(data.createdDate);
                     }
                 }
+                if(moment(data.createdDate).week() - (moment(d3.min(datesAggregated)).week()) == i-1){
+                    if(moment(data.createdDate).day() == 6){
+                        tempLevels[7][1].push(data.level);
+                        tempTimes[7][1].push(data.createdTime);
+                        tempDates[7][1].push(data.createdDate);
+                    }
+                }
+
+
             });
+
             structured.push({
                 'week': i+1,
+                'prevsat': {'dates': tempDates[7][1], 'times': tempTimes[7][1], 'levels': tempLevels[7][1], 'count': tempDates[7][1].length},
                 'sunday': {'dates': tempDates[0][1], 'times': tempTimes[0][1], 'levels': tempLevels[0][1], 'count': tempDates[0][1].length},
                 'monday': {'dates': tempDates[1][1], 'times': tempTimes[1][1], 'levels': tempLevels[1][1], 'count': tempDates[1][1].length},
                 'tuesday': {'dates': tempDates[2][1], 'times': tempTimes[2][1], 'levels': tempLevels[2][1], 'count': tempDates[2][1].length},
                 'wednesday': {'dates': tempDates[3][1], 'times': tempTimes[3][1], 'levels': tempLevels[3][1], 'count': tempDates[3][1].length},
                 'thursday': {'dates': tempDates[4][1], 'times': tempTimes[4][1], 'levels': tempLevels[4][1], 'count': tempDates[4][1].length},
                 'friday': {'dates': tempDates[5][1], 'times': tempTimes[5][1], 'levels': tempLevels[5][1], 'count': tempDates[5][1].length},
-                'saturday': {'dates': tempDates[6][1], 'times': tempTimes[6][1], 'levels': tempLevels[6][1], 'count': tempDates[6][1].length}});
+                'saturday': {'dates': tempDates[6][1], 'times': tempTimes[6][1], 'levels': tempLevels[6][1], 'count': tempDates[6][1].length}
+            });
         }
 
 
-    }
-
-    var catColors = d3.scale.category10(); // color function
-
-    // shape mode
-    function drawShapeCanvas(svg, id, numOfWeeks){
-        if(!numOfWeeks){
-            numOfWeeks = 1;
-        }
-        // expands the svg height according to the # of weeks to be displayed
-        if(mode == 0){
-            svg.style({'height': svgHeight*numOfWeeks + 'px' });
-        }else if(mode == 1 || mode == 2){
-            svg.style({'height': svgHeight + 'px' });
-        }
-
-
-        svg.append('text')
-            .text(function(){ var map=["SUNDAY","MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY","SATURDAY"]; return map[svg.attr('id')];})
-            .attr({x: svgPadding*1-10, y: svgPadding-35, 'transform': 'rotate(90)', 'font-size': 25})
-            .style({'fill':'#666666', 'stroke-width':0});
-
-        for(var i=0; i<numOfWeeks; i++){
-            svg.append('rect')
-                .attr({x: svgPadding*8, y: svgPadding-10 + i * 39, width: svgWidth-10*svgPadding, height: 29 })
-                .style({'stroke': '#ddd', 'stroke-width': 0, 'fill': 'none'});
-
-            for(var j=0; j<23; j++){
-                svg.append('rect')
-                    .attr({x: svgPadding*8 + (svgWidth-10*svgPadding)/24 * j, y: svgPadding-10 + i * 39, width: (svgWidth-10*svgPadding)/24, height: 29 })
-                    .style({'stroke': '#ddd', 'stroke-width': 0, 'fill': 'none'})
-            }
-
-            svg.append('rect')
-                .attr({x: scaleTimeHeatmap("06:00 AM", "2013-08-10"), y: svgPadding-10, width: scaleTimeHeatmap("09:00 AM", "2013-08-10") - scaleTimeHeatmap("06:00 AM", "2013-08-10"), "class": "breakfastBracket", "height": 147})
-                .style({"fill": "#eeeeee"});
-
-            svg.append('rect')
-                .attr({x: scaleTimeHeatmap("11:00 AM", "2013-08-10"), y: svgPadding-10, width: scaleTimeHeatmap("02:00 PM", "2013-08-10") - scaleTimeHeatmap("11:00 AM", "2013-08-10"), "class": "lunchBracket", "height": 147})
-                .style({"fill": "#eeeeee"});
-
-            svg.append('rect')
-                .attr({x: scaleTimeHeatmap("05:00 PM", "2013-08-10"), y: svgPadding-10, width: scaleTimeHeatmap("08:00 PM", "2013-08-10") - scaleTimeHeatmap("05:00 PM", "2013-08-10"), "class": "dinnerBracket", "height": 147})
-                .style({"fill": "#eeeeee"});
-
-        }
-
-        var ticksTime = ["12:00 AM","3:00 AM","6:00 AM","9:00 AM","12:00 PM","3:00 PM","6:00 PM","9:00 PM","11:59 PM"];
-
-
-        svg.selectAll('text.time').data(ticksTime).enter().append('text')
-            .text(function(d,i){ if(i != 8){ return moment('2013/01/01 '+d).format('h A');}else{ return (moment('2013/01/01 '+d).hour()+1)/2+ ' AM'; } })
-            .attr({'x': function(d,i){ return svgPadding*8+(svgWidth-10*svgPadding)/24*(i)*3-12; }, 'y': svgHeight - svgPadding*2 + 30, 'font-size': '9px', 'class': 'time'});
     }
 
     function drawShape(svg, numOfWeeks, day){
@@ -193,29 +160,43 @@ define(['jquery','D3','queue','moment','slider'], function($, d3, queue, moment)
             numOfWeeks = 1;
         }
 
-        for(var j=0; j<numOfWeeks; j++){
-            var testDrive = eval('structured[j].'+day);
-            var oneDate = new Array(testDrive.dates[0]);
+        var currentWeek = parseInt(svg.attr('id'));
+        var date = [];
+        var mapDay = {0: "Sun", 1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat"};
 
-            svg.selectAll('rect.weekIndicator'+j).data(oneDate).enter().append('rect')
-                .attr({'x': svgPadding*3+10, 'y': 10 + j*39, 'width': 5, 'height': 29, 'class': 'weekIndicator'+j})
-                .style({'fill': function(d){ if(d != undefined){ return catColors(j)}else{ return '#ffffff';}}});
-            svg.selectAll('text.monthIndicator'+j).data(oneDate).enter().append('text')
-                .attr({'x': svgPadding*3+24, 'y': 30 + j*39, 'width': svgPadding*2, 'height': svgPadding*2, 'font-size': 14, 'class': 'monthIndicator'+j})
-                .text(function(d,i){ if(d != undefined){ return moment(d).format('MMM') }else{ return null;}})
-                .style({'fill': '#999999', 'stroke-width': 0});
-            svg.selectAll('text.dayIndicator'+j).data(oneDate).enter().append('text')
-                .attr({'x': svgPadding*3+55, 'y': 30 + j*39, 'width': svgPadding*2, 'height': svgPadding*2, 'font-size': 14, 'class': 'dayIndicator'+j})
-                .text(function(d,i){ if(d != undefined){ return moment(d).format('DD') }else{ return null;}})
-                .style({'fill': '#999999', 'stroke-width': 0});
+        svg.append('g').attr({'class': 'testGraph'+currentWeek});
+        console.log(structured);
 
-            svg.append('g').attr({'class': 'testGraph'+j});
-            svg.select('g.testGraph'+j).selectAll('g').data(testDrive.levels).enter().append('g')
+        var mapD = {0: "prevsat", 1: "sunday", 2: "monday", 3: "tuesday", 4: "wednesday", 5: "thursday", 6: "friday", 7: "saturday"};
+        var dates = [];
+
+        for(var i=0; i<8; i++){
+            dates.push(eval("structured[currentWeek]."+mapD[i]+".dates[0]"));
+
+        }
+
+        svg.selectAll('text.monthIndicator').data(dates).enter().append('text')
+            .attr({'x': svgPadding*5+35, 'y': function(d,i){ return 30+39*i; }, 'width': svgPadding*2, 'height': svgPadding*2, 'font-size': 12, 'class': 'monthIndicator'})
+            .text(function(d,i){ if(d != undefined){ return moment(d).format('MMM, DD') }else{ return null;}})
+            .attr("text-anchor","end")
+            .style({'fill': '#666', 'stroke-width': 1, 'stroke': '#666'});
+
+        svg.selectAll('text.dayIndicator').data(dates).enter().append('text')
+            .attr({'x': svgPadding*3+20, 'y': function(d,i){ return 30+39*i; }, 'width': svgPadding*2, 'height': svgPadding*2, 'font-size': 12, 'class': 'dayIndicator'})
+            .text(function(d,i){ if(d){ return mapDay[moment(d).day()]; } })
+            .attr("text-anchor","end")
+            .style({'fill': '#666', 'stroke-width': 1, 'stroke': '#666'});
+
+        svg.append('g').attr({'class': 'testGraph'+currentWeek});
+
+        for(var i=0; i<8; i++){
+            var testDrive = eval('structured[currentWeek].'+mapD[i]);
+            svg.select('g.testGraph'+currentWeek).selectAll('g').data(testDrive.levels).enter().append('g')
                 .attr({'class': 'elementGroup'});
-            svg.select('g.testGraph'+j).selectAll('.elementGroup').data(testDrive.levels).append('path')
+            svg.select('g.testGraph'+currentWeek).selectAll('.elementGroup').data(testDrive.levels).append('path')
                 .attr({
-                    'd': function(d,i){
-                        var dPath = 'M '+parseInt(scaleTimeHeatmap(testDrive.times[i], testDrive.dates[0])-((svgWidth-10*svgPadding)/24/2))+' '+parseInt(svgPadding-10 + j*39);
+                    'd': function(d,idx){
+                        var dPath = 'M '+parseInt(scaleTimeHeatmap(testDrive.times[idx], testDrive.dates[0])-((svgWidth-10*svgPadding)/24/2))+' '+parseInt(svgPadding-10 + i*39);
                         if(d>180){ // draw circle
                             dPath += ' m 0, 14.5 a 14.5,14.5 0 1, 0 29, 0 a 14.5,14.5 0 1,0 -29,0';
                         }else if(d<=180 && d>70){
@@ -232,22 +213,118 @@ define(['jquery','D3','queue','moment','slider'], function($, d3, queue, moment)
                 .style({'stroke': 'rgba(180,180,180,0.8)', 'stroke-width': 1 });
 
             if(showValue == 1){
-                svg.select('g.testGraph'+j).selectAll('.elementGroup').data(testDrive.levels).append('text')
-                    .attr({'x': function(d,i){ return scaleTimeHeatmap(testDrive.times[i], testDrive.dates[0])-(svgWidth-10*svgPadding)/24/2 + 3;}, 'y': svgPadding+ 10 + j*39, 'font-size': 9, 'id': function(d,i){ return i;}, 'class': 'BGValue'})
+                svg.select('g.testGraph'+currentWeek).selectAll('.elementGroup').data(testDrive.levels).append('text')
+                    .attr({'x': function(d,idx){ return scaleTimeHeatmap(testDrive.times[idx], testDrive.dates[0])-(svgWidth-10*svgPadding)/24/2 + 3;}, 'y': svgPadding+ 10 + i*39, 'font-size': 9, 'id': function(d,i){ return i;}, 'class': 'BGValue'})
                     .text(function(d,i){ return d;})
                     .style({'fill': '#000000', 'stroke-width': 0, 'stroke': '#000000'});
             }
+
         }
 
-        svg.selectAll('.elementGroup').on('mouseover', function(d,i){ // svg.selectAll('g rect').on('mouseover', function(d,i){
+        svg.selectAll('.BGValue').on('mouseover', function(d,i){ // svg.selectAll('g rect').on('mouseover', function(d,i){
 
-            d3.select(this).select('.BGValue').transition().attr({'font-size': 20}); // 'y': parseInt(d3.select(textElement)[0][0].attr('y')) - 20,
+            d3.select(this).transition().attr({'font-size': 20}); // 'y': parseInt(d3.select(textElement)[0][0].attr('y')) - 20,
         });
 
-        svg.selectAll('.elementGroup').on('mouseout', function(d,i){
+        svg.selectAll('.BGValue').on('mouseout', function(d,i){
             var textElement = $(this).parent().find('.BGValue[id='+$(this).attr('id')+']');
-            d3.select(this).select('.BGValue').transition().attr({ 'font-size': 9}); //'y': parseInt(d3.select(textElement)[0][0].attr('y')) + 20,
+            d3.select(this).transition().attr({ 'font-size': 9}); //'y': parseInt(d3.select(textElement)[0][0].attr('y')) + 20,
         });
+
+
+
+
+
+//        for(var j=0; j<numOfWeeks; j++){
+//            var testDrive = eval('structured[j].'+day);
+//            var oneDate = new Array(testDrive.dates[0]);
+//
+
+//
+//            svg.append('g').attr({'class': 'testGraph'+j});
+//            svg.select('g.testGraph'+j).selectAll('g').data(testDrive.levels).enter().append('g')
+//                .attr({'class': 'elementGroup'});
+//            svg.select('g.testGraph'+j).selectAll('.elementGroup').data(testDrive.levels).append('path')
+//                .attr({
+//                    'd': function(d,i){
+//                        var dPath = 'M '+parseInt(scaleTimeHeatmap(testDrive.times[i], testDrive.dates[0])-((svgWidth-10*svgPadding)/24/2))+' '+parseInt(svgPadding-10 + j*39);
+//                        if(d>180){ // draw circle
+//                            dPath += ' m 0, 14.5 a 14.5,14.5 0 1, 0 29, 0 a 14.5,14.5 0 1,0 -29,0';
+//                        }else if(d<=180 && d>70){
+//                            dPath += ' m 0, 14.5 a 14.5,14.5 0 1, 0 29, 0 a 14.5,14.5 0 1,0 -29,0'; // m 14.5, 29 l 14.5 0 l 0 -29 l -29 0 l 0 29 l 14.5 0';
+//                        }else{
+//                            dPath += ' m 14.5, 0 l -14.5 29 l 29 0 l -14.5 -29';
+//                        }
+//                        return dPath;},
+//                    'id': function(d,i){
+//                        return i;}
+//                })
+//                .attr({"stroke-dasharray":function(d,i){ if(d<=180 && d>70){ return "5,5" }else{ return ""} }})
+//                .style({'fill': function(d,i){ if(d>180){ return "rgba(200,200,200,0.8)"; }else{ return "#ffffff"}}})
+//                .style({'stroke': 'rgba(180,180,180,0.8)', 'stroke-width': 1 });
+//
+//            if(showValue == 1){
+//                svg.select('g.testGraph'+j).selectAll('.elementGroup').data(testDrive.levels).append('text')
+//                    .attr({'x': function(d,i){ return scaleTimeHeatmap(testDrive.times[i], testDrive.dates[0])-(svgWidth-10*svgPadding)/24/2 + 3;}, 'y': svgPadding+ 10 + j*39, 'font-size': 9, 'id': function(d,i){ return i;}, 'class': 'BGValue'})
+//                    .text(function(d,i){ return d;})
+//                    .style({'fill': '#000000', 'stroke-width': 0, 'stroke': '#000000'});
+//            }
+//        }
+
+
+    }
+
+    var catColors = d3.scale.category10(); // color function
+
+    // shape mode
+    function drawShapeCanvas(svg, id, numOfWeeks){
+        if(!numOfWeeks){
+            numOfWeeks = 1;
+        }
+        // expands the svg height according to the # of days (7) to be displayed
+        if(mode == 0){
+            svg.style({'height': svgHeight * 7+ 'px' });
+        }else if(mode == 1 || mode == 2){
+            svg.style({'height': 39 * 9 + 'px' });
+        }
+
+        svg.select(".weekIndicator").remove();
+        svg.append('text')
+            .text(function(){ return "Week " + parseInt(parseInt(svg.attr('id'))+1); })
+            .attr({x: svgPadding*1-10, y: svgPadding-35, 'transform': 'rotate(90)', 'font-size': 25, "class": "weekIndicator"})
+            .style({'fill':'#666666', 'stroke-width':0});
+
+        for(var i=0; i<8; i++){
+            svg.append('rect')
+                .attr({x: svgPadding*8, y: svgPadding-10 + i * 39, width: svgWidth-10*svgPadding, height: 29 })
+                .style({'stroke': '#ddd', 'stroke-width': 0, 'fill': 'none'});
+
+            for(var j=0; j<23; j++){
+                svg.append('rect')
+                    .attr({x: svgPadding*8 + (svgWidth-10*svgPadding)/24 * j, y: svgPadding-10 + i * 39, width: (svgWidth-10*svgPadding)/24, height: 29 })
+                    .style({'stroke': '#ddd', 'stroke-width': 0, 'fill': 'none'})
+            }
+
+            svg.append('rect')
+                .attr({x: scaleTimeHeatmap("06:00 AM", "2013-08-10"), y: svgPadding-10, width: scaleTimeHeatmap("09:00 AM", "2013-08-10") - scaleTimeHeatmap("06:00 AM", "2013-08-10"), "class": "breakfastBracket", "height": 147+156})
+                .style({"fill": "#eeeeee"});
+
+            svg.append('rect')
+                .attr({x: scaleTimeHeatmap("11:00 AM", "2013-08-10"), y: svgPadding-10, width: scaleTimeHeatmap("02:00 PM", "2013-08-10") - scaleTimeHeatmap("11:00 AM", "2013-08-10"), "class": "lunchBracket", "height": 147+156})
+                .style({"fill": "#eeeeee"});
+
+            svg.append('rect')
+                .attr({x: scaleTimeHeatmap("05:00 PM", "2013-08-10"), y: svgPadding-10, width: scaleTimeHeatmap("08:00 PM", "2013-08-10") - scaleTimeHeatmap("05:00 PM", "2013-08-10"), "class": "dinnerBracket", "height": 147+156})
+                .style({"fill": "#eeeeee"});
+
+        }
+
+        var ticksTime = ["12:00 AM","3:00 AM","6:00 AM","9:00 AM","12:00 PM","3:00 PM","6:00 PM","9:00 PM","11:59 PM"];
+
+
+        svg.selectAll('text.time').data(ticksTime).enter().append('text')
+            .text(function(d,i){ if(i != 8){ return moment('2013/01/01 '+d).format('h A');}else{ return (moment('2013/01/01 '+d).hour()+1)/2+ ' AM'; } })
+            .attr({'x': function(d,i){ return svgPadding*8+(svgWidth-10*svgPadding)/24*(i)*3-12; }, 'y': svgHeight - svgPadding*2 + 195, 'font-size': '9px', 'class': 'time'});
     }
 
     // heatmap mode
@@ -534,17 +611,25 @@ define(['jquery','D3','queue','moment','slider'], function($, d3, queue, moment)
 
         var svgs = [];
         var map = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"];
-        for(var i=0; i<7; i++){
+//        for(var i=0; i<7; i++){
+//            svgs[i] = d3.select('.vizElement').append('svg').style({'width': svgWidth+'px', 'height': svgHeight+'px' }).attr({'id': i});
+//        }
+
+        for(var i=0; i<weeksToDisplay; i++){
             svgs[i] = d3.select('.vizElement').append('svg').style({'width': svgWidth+'px', 'height': svgHeight+'px' }).attr({'id': i});
         }
 
         $('svg').after("<hr>");
 
-        for(var i=0; i<7; i++){
-
-//            drawScatterCanvas(svgs[i], $(svgs[i]).attr('id'), weeksToDisplay);
+//        for(var i=0; i<7; i++){
+//
+////            drawScatterCanvas(svgs[i], $(svgs[i]).attr('id'), weeksToDisplay);
+//            drawCanvas(svgs[i], $(svgs[i]).attr('id'), weeksToDisplay);
+////            drawScatter(svgs[i], weeksToDisplay, map[i]);
+//            draw(svgs[i], weeksToDisplay, map[i]);
+//        }
+        for(var i=0; i<weeksToDisplay; i++){
             drawCanvas(svgs[i], $(svgs[i]).attr('id'), weeksToDisplay);
-//            drawScatter(svgs[i], weeksToDisplay, map[i]);
             draw(svgs[i], weeksToDisplay, map[i]);
         }
 
